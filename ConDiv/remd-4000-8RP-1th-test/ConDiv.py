ff2c05eb75e4ac31f068813dc0d8ed328871d06c
Yinhan <ywang268@jhu.edu>
2026-01-04 21:07:15 -0600

    Sat Dec 27 22:28:55 CST 2025

--- ConDiv/remd-4000-8RP-1th-test/ConDiv.py
+++ ConDiv/remd-4000-8RP-1th-test/ConDiv.py
@@ -877,7 +877,7 @@ def main_initialize(args):
     state['initial_alpha'] = Update(*[
             0.1, 0., 0.5, 0., 0.02, 0.03])
     #state['initial_alpha'] = state['initial_alpha'] * 0.025
-    state['initial_alpha'] = state['initial_alpha'] * 0.05
+    state['initial_alpha'] = state['initial_alpha'] * 0.025
     state['solver'] = rp.AdamSolver(len(state['initial_alpha']), alpha=state['initial_alpha']) 
     state['sim_time'] = 4000
 
