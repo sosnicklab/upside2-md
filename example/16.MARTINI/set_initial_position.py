a17b8e125bc2731fcb3a45df1ff8e06b1213b3c9
Yinhan <ywang268@jhu.edu>
2026-02-08 02:58:54 -0600

    Sat Feb  7 02:18:40 CST 2026

--- example/16.MARTINI/set_initial_position.py
+++ example/16.MARTINI/set_initial_position.py
@@ -7,8 +7,14 @@ import numpy as np
 def set_initial_position(input_file, output_file):
     # Open input file and get last frame's position and box dimensions
     with h5py.File(input_file, 'r') as f:
-        last_pos = f['/output/pos'][-1, 0, :, :]  # Last frame, first replica, all atoms, all coordinates
-        last_pos = last_pos[:, :, np.newaxis]  # Add replica dimension (1)
+        # Try to read from output/pos (if simulation has been run)
+        if '/output/pos' in f:
+            last_pos = f['/output/pos'][-1, 0, :, :]  # Last frame, first replica, all atoms, all coordinates
+            last_pos = last_pos[:, :, np.newaxis]  # Add replica dimension (1)
+        else:
+            # If no output/pos, read from input/pos (initial position)
+            last_pos = f['/input/pos'][:, :, 0]  # First frame from input
+            last_pos = last_pos[:, :, np.newaxis]  # Add replica dimension (1)
 
         # Get last frame's box dimensions if available
         last_box = None
@@ -19,6 +25,16 @@ def set_initial_position(input_file, output_file):
                 # Handle different box data formats
                 if len(last_box.shape) == 2 and last_box.shape[1] == 3:
                     last_box = last_box[0]
+        # If no output/box, try to get box from input potential attributes
+        if last_box is None:
+            if '/input/potential/martini_potential' in f:
+                pot_grp = f['/input/potential/martini_potential']
+                if all(k in pot_grp.attrs for k in ('x_len', 'y_len', 'z_len')):
+                    last_box = np.array([
+                        pot_grp.attrs['x_len'],
+                        pot_grp.attrs['y_len'],
+                        pot_grp.attrs['z_len']
+                    ])
 
     # Open output file and write last frame's position to input/pos
     with h5py.File(output_file, 'r+') as f:
